71597b41ac9ad7b0df93146c22b5a2732b517b88
ReptileSD <denbakushev@gmail.com>
2020-12-19 12:33:35 +1000

    grab stones

---
+++ Assets/Scripts/Grabbable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+public class Grabbable : MonoBehaviour
+{
+    public int touchCount;
+    void start()
+    {
+        if (gameObject.tag != "Grabbable")
+        {
+            Debug.LogError("Interactable's tag is not set to grabbable");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        touchCount++;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        touchCount--;
+    }
+}
